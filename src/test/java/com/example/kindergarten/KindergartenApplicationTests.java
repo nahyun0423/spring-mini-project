be50cf8eec279e78e6de8602e8eb2daa523c1181
nahyun0423 <skgus0423@naver.com>
2026-01-03 15:40:03 +0900

    Fix CI test profile (H2)

--- src/test/java/com/example/kindergarten/KindergartenApplicationTests.java
+++ src/test/java/com/example/kindergarten/KindergartenApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.kindergarten;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class KindergartenApplicationTests {
 
